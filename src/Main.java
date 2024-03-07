91164093dc4cff72c453ac212ba821876f886276
Soprodevento <Sashinka2803@gmail.com>
2024-03-08 00:56:35 +0300

    код стайл

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        BigInteger  result = factorial(3);
+        BigInteger result = factorial(3);
         System.out.println(result);
     }
 
-    public static BigInteger  factorial(int value) {
-        BigInteger  n = BigInteger.ONE;
-        for (int i = 1; i <= value; i++){
+    public static BigInteger factorial(int value) {
+        BigInteger n = BigInteger.ONE;
+        for (int i = 1; i <= value; i++) {
             n = n.multiply(BigInteger.valueOf(i));
         }
         return n;
