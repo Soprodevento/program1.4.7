f4e3cb9e402c5a0d8bd2301370060f656784d9db
Soprodevento <Sashinka2803@gmail.com>
2024-03-07 21:01:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+public class Main {
+    public static void main(String[] args) {
+        BigInteger  result = factorial(3);
+        System.out.println(result);
+    }
+
+    public static BigInteger  factorial(int value) {
+        BigInteger  n = BigInteger.ONE;
+        for (int i = 1; i <= value; i++){
+            n = n.multiply(BigInteger.valueOf(i));
+        }
+        return n;
+    }
+}
